5b6d0a5be6d947e9a0c117372ed57de6dd4a5c86
soum-sr <soum1071@gmail.com>
2020-06-09 23:34:49 +0530

    updated view

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 # Function to handle the traffic from our home
 # also in this place we decide how we want to handle to routes
 
-posts = [
-    {
-        'author':'Soumyajit Rout',
-        'title':'Blog post 1',
-        'content': 'First post content',
-        'date_posted':'June 4 2020'
-    },
-    {
-        'author':'Suman Goel',
-        'title':'Blog post 2',
-        'content': 'Second post content',
-        'date_posted':'June 5 2020'
-    },
-]
 
 def home(request):
     context = {
-        'posts':posts,
+        'posts':Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
     
