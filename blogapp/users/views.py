b26defe9024f96cf139c30d3917e154658020a6d
soum-sr <soum1071@gmail.com>
2020-06-12 23:52:02 +0530

    updated user

--- blogapp/users/views.py
+++ blogapp/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # To display flash messages
 from django.contrib import messages
 
@@ -16,13 +17,15 @@ def register(request):
             form.save() # thats it!
             username = form.cleaned_data.get('username')
             # Flash message
-            messages.success(request, f'Account Created for {username}!')
-            return redirect('blog-home') # takes the name of url
-
-
+            messages.success(request, f'Account Created for {username}. You can now Log In!')
+            return redirect('login') # takes the name of url
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html',{'form':form})
 
 
+# Decorator to make sure that this route is only accessible when the user is logged in
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
