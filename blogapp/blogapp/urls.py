f9c76041f5774d6e00eb2686b46e43c8745418fa
soum-sr <soum1071@gmail.com>
2020-06-13 00:00:00 +0530

    added profile

--- blogapp/blogapp/urls.py
+++ blogapp/blogapp/urls.py
@@ -18,6 +18,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include # include- it includes the app specific urls to the project
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -25,5 +28,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # class based views to handle the logics(not the template)
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')), # here we include urls file from our blog app
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
