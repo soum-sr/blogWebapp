113e6d2ac25e3f2293560be3d5954e478a118146
soum-sr <soum1071@gmail.com>
2020-06-16 00:07:23 +0530

    added create,update,delete

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # Create your views here.
@@ -23,14 +24,16 @@ posts = [
     },
 ]
 
+# Function based views
+
+# def home(request):
+#     context = {
+#         'posts':Post.objects.all(),
+#     }
+#     return render(request, 'blog/home.html', context)
+    
 # Classed based views
 
-def home(request):
-    context = {
-        'posts':Post.objects.all(),
-    }
-    return render(request, 'blog/home.html', context)
-    
 class PostListView(ListView):
     model = Post # The model we are working on 
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html {default one}
@@ -41,13 +44,41 @@ class PostDetailView(DetailView):
     model = Post # The model we are working on 
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post # The model we are working on 
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = Post # The model we are working on 
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True 
+        
+        return False        
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post # The model we are working on 
+    success_url = '/' # redirects to home page when page is deleted
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True 
+        
+        return False        
+
+
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
