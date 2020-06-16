4a40e8df093efe5e4263ac8e17a1d0ea9eb4285c
soum-sr <soum1071@gmail.com>
2020-06-16 15:51:25 +0530

    added userpage

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
@@ -40,7 +41,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html {default one}
     context_object_name = 'posts' # Value p assed to the template
     ordering = ['-date_posted'] # It decides the sorting
-    paginate_by = 2
+    paginate_by = 3
+
+# For listing all posts of a single user
+class UserPostListView(ListView):
+    model = Post # The model we are working on 
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html {default one}
+    context_object_name = 'posts' # Value p assed to the template
+    
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+        
 
 
 class PostDetailView(DetailView):
